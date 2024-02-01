0eff2a4df3664730a5f7b07c097e3ec0697dcc33
ZAKARIAE ELMAJDOULI <99546678+SploitGuy@users.noreply.github.com>
2024-02-01 08:35:54 +0100

    Add files via upload

---
+++ fingerprinting_webservers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import requests
+
+
+req = requests.get('http://10.0.3.8')
+headers = ['Server', 'Date', 'Via', 'X-Powered-By', 'X-Country-Code']
+for header in headers:
+    try:
+        result = req.headers[header]
+        print('%s: %s' % (header, result))
+    except:
+        print ('%s: Not found' % header)
